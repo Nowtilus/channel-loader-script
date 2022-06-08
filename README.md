50f914a5a54e0afddfa1bf4bab49b33f13d51cb0
Ingo Eichhorst <ingo.eichhorst@nowtilus.tv>
2022-06-08 15:14:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Channel Loader Script
+
+This script takes a channel from one ssai-cluster and adds it to multiple other clusters. That is espacially usefull in a multi-cluster setup.
+
+Note: The cluster need to have the same URL. The only thing that is allowed to change is the ID. (see CLUSTER_URL_TEMPLATE below)
+
+## Prerequisites
+
+1. Install NodeJS
+2. Fetch this script
+
+```bash
+  git clone 
+```
+
+3. Install Dependencies
+
+```bash
+  npm i
+```
+
+## Config and usage
+
+Config params:
+
+- CHANNEL_ID - ID of the channel that is available in the source cluster and that you want to insert or update in the otehr clusters
+- USERNAME - User to access the channel-api of the source or target cluster
+- PASSWORD - Password to access the channel-api of the source or target cluster
+- CLUSTER_RANGE - Range of the clusters to update in the form of "first..last" (e.g. 1..5 will update the cluster 1 to 5 with the data from 0)
+- CLUSTER_URL_TEMPLATE - A url that points to the channel-api endpoints of the clusters. The number defined in the range will be inserted at the placeholder "{}"
+
+```bash
+  CHANNEL_ID="8111626d-f697-4330-887a-f2be0b4f2127" \
+  USERNAME="nowtilus" \
+  PASSWORD="your_cluster_api_password" \
+  CLUSTER_RANGE="1..5" \
+  CLUSTER_URL_TEMPLATE="http://api.ssai-{}.jio.serverside.ai/channel-api/api/v1/channels" \
+  node index.js
+```
---
+++ index.js
@@ -0,0 +1,71 @@
+import fetch from "node-fetch";
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+// Loading Config
+const channelId = process.env.CHANNEL_ID
+if(!channelId) throw new Error('CHANNEL_ID is not set')
+const password = process.env.PASSWORD
+if(!password) throw new Error('PASSWORD is not set')
+const username = process.env.USERNAME
+if(!username) throw new Error('USERNAME is not set')
+const clusterRange = process.env.CLUSTER_RANGE.split('..') // 1..5
+if(!clusterRange || clusterRange.length !== 2) throw new Error('USERNAME is not set')
+const firstCluster = clusterRange[0]
+const lastCluster = clusterRange[1]
+
+const clusterUrlTemplate = process.env.CLUSTER_URL_TEMPLATE // http://api.ssai-{}.serverside.ai/blablabla
+if(!clusterUrlTemplate) throw new Error('USERNAME is not set')
+
+const sourceUrl = clusterUrlTemplate.replace('{}', '0')
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Basic ' + Buffer.from(username + ':' + password).toString('base64')
+}
+
+function loadChannel () {
+  run()
+}
+
+async function run () {
+  try {
+    const response = await fetch(sourceUrl + '/' + channelId, {
+      method: 'GET', 
+      headers
+    })
+    if(response.status !== 200) throw new Error('Failed to get channel', response.status)
+    const channel = await response.json()
+    console.log('LOADED CHANNEL:', JSON.stringify(channel))
+    console.log('UPDATING CHANNEL:', channelId)
+    
+    for(let i = firstCluster; i <= lastCluster; i++) {
+      const clusterUrl = clusterUrlTemplate.replace('{}', i)
+      
+      console.log('UPDATING CLUSTER:', clusterUrl)
+
+      const deleteResponse = await fetch(clusterUrl + '/' + channelId, {
+        method: 'DELETE',
+        headers
+      })
+      if(deleteResponse.status !== 200) throw new Error('Failed to delete channel', deleteResponse.status)
+      console.log('DELETED:', channelId)
+
+      await sleep(1000)
+
+      const createResponse = await fetch(clusterUrl, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(channel),
+      })
+
+      if(createResponse.status !== 201) throw new Error('Failed to create channel', createResponse.status)
+      const data = await createResponse.json()
+      console.log('UPDATED CHANNEL IN CLUSTER:', clusterUrl)
+    }
+    console.log('FINISHED UPDATING ALL CLUSTERS')
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+run()
