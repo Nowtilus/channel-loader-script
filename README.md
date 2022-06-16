ddee16b595a0b6e8a2cb2bd73f3c0f19adcf0ad6
Ingo Eichhorst <ingo.eichhorst@nowtilus.tv>
2022-06-16 11:42:36 +0200

    added handling of multiple Ids

--- README.md
+++ README.md
@@ -23,14 +23,14 @@ Note: The cluster need to have the same URL. The only thing that is allowed to c
 
 Config params:
 
-- CHANNEL_ID - ID of the channel that is available in the source cluster and that you want to insert or update in the otehr clusters
+- CHANNEL_ID - Comma seperated list of channelIds that are available in the source cluster and that you want to insert or update in the otehr clusters
 - USERNAME - User to access the channel-api of the source or target cluster
 - PASSWORD - Password to access the channel-api of the source or target cluster
 - CLUSTER_RANGE - Range of the clusters to update in the form of "first..last" (e.g. 1..5 will update the cluster 1 to 5 with the data from 0)
 - CLUSTER_URL_TEMPLATE - A url that points to the channel-api endpoints of the clusters. The number defined in the range will be inserted at the placeholder "{}"
 
 ```bash
-  CHANNEL_ID="8111626d-f697-4330-887a-f2be0b4f2127" \
+  CHANNEL_ID="8111626d-f697-4330-887a-f2be0b4f2127,8111626d-f697-4330-887a-f2be0b4f2127" \
   API_USER="nowtilus" \
   PASSWORD="your_cluster_api_password" \
   CLUSTER_RANGE="1..5" \
--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import fetch from "node-fetch";
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 // Loading Config
-const channelId = process.env.CHANNEL_ID
-if(!channelId) throw new Error('CHANNEL_ID is not set')
+const ids = process.env.CHANNEL_ID
+if(!ids) throw new Error('CHANNEL_ID is not set')
 const password = process.env.PASSWORD
 if(!password) throw new Error('PASSWORD is not set')
 const username = process.env.API_USER
@@ -18,7 +18,7 @@ if(!clusterUrlTemplate) throw new Error('USERNAME is not set')
 
 const sourceUrl = clusterUrlTemplate.replace('{}', '0')
 
-console.log(channelId, password, username, clusterRange, clusterUrlTemplate, sourceUrl)
+// console.log(channelId, password, username, clusterRange, clusterUrlTemplate, sourceUrl)
 
 const headers = {
   'Content-Type': 'application/json',
@@ -29,55 +29,75 @@ function loadChannel () {
   run()
 }
 
-async function run () {
-  try {
-    console.log(sourceUrl + '/' + channelId)
-    const response = await fetch(sourceUrl + '/' + channelId, {
-      method: 'GET', 
-      headers
-    })
-    if(response.status !== 200) throw new Error('Failed to get channel: ' + response.status)
-    const channel = await response.json()
-    console.log('LOADED CHANNEL:', JSON.stringify(channel))
-    console.log('UPDATING CHANNEL:', channelId)
-    
-    for(let i = firstCluster; i <= lastCluster; i++) {
-      const clusterUrl = clusterUrlTemplate.replace('{}', i)
-      
-      console.log('UPDATING CLUSTER:', clusterUrl)
+const channeIds = ids.split(",")
 
-      const deleteResponse = await fetch(clusterUrl + '/' + channelId, {
-        method: 'DELETE',
+async function run () {
+  for (const channelId of channeIds) {    
+    console.log('UPDATING CHANNEL:', channelId)
+    try {
+      console.log(sourceUrl + '/' + channelId)
+      const response = await fetch(sourceUrl + '/' + channelId, {
+        method: 'GET', 
         headers
       })
-      if(![200,404].includes(deleteResponse.status)) throw new Error('Failed to delete channel: ' + deleteResponse.status)
-      console.log('DELETED:', channelId)
-
-      await sleep(2000)
+      if(response.status !== 200) throw new Error('Failed to get channel: ' + response.status)
+      const channel = await response.json()
+      console.log('LOADED CHANNEL:', JSON.stringify(channel))
       
-      const createResponse = await fetch(clusterUrl, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify(channel),
-      })
-      if(createResponse.status !== 201) throw new Error('Failed to create channel: ' + createResponse.status)
-      const data = await createResponse.json()
-      console.log('UPDATED CHANNEL IN CLUSTER:', clusterUrl)
-      
-      await sleep(2000)
-      const enableResponse = await fetch(clusterUrl + '/' + channelId, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({"state": "enabled"}),
-      })
-      const enableData = await enableResponse.text()
-      if(enableResponse.status !== 200) throw new Error('Failed to enable channel: ' + enableResponse.status + ' - ' + enableData)
-      console.log('ENABLED CHANNEL IN CLUSTER:', clusterUrl)
+      for(let i = firstCluster; i <= lastCluster; i++) {
+        const clusterUrl = clusterUrlTemplate.replace('{}', i)
+        
+        console.log('UPDATING CLUSTER:', clusterUrl)
+  
+        const deleteResponse = await fetch(clusterUrl + '/' + channelId, {
+          method: 'DELETE',
+          headers
+        })
+        if(![200,404].includes(deleteResponse.status)) throw new Error('Failed to delete channel: ' + deleteResponse.status)
+        console.log('DELETED:', channelId)
+  
+        await sleep(2000)
+        
+        const createResponse = await fetch(clusterUrl, {
+          method: 'POST',
+          headers,
+          body: JSON.stringify(channel),
+        })
+        if(createResponse.status !== 201) throw new Error('Failed to create channel: ' + createResponse.status)
+        const data = await createResponse.json()
+        console.log('UPDATED CHANNEL IN CLUSTER:', clusterUrl)
+        
+        await sleep(2000)
+        const enableResponse = await fetch(clusterUrl + '/' + channelId, {
+          method: 'POST',
+          headers,
+          body: JSON.stringify({"state": "enabled"}),
+        })
+        const enableData = await enableResponse.text()
+        if(enableResponse.status !== 200) throw new Error('Failed to enable channel: ' + enableResponse.status + ' - ' + enableData)
+        console.log('ENABLED CHANNEL IN CLUSTER:', clusterUrl)
+  
+        // console.log('Waiting 10s For Channel to be created ...')
+        // await sleep(30000)
+        // // Check if it's playing
+        // // https://live.ssai-" + std::to_string(num) + ".jio.serverside.ai" + path
+        // const avilCheckUrl = "https://live.ssai-" + i + ".jio.serverside.ai/view/" + channelId + '?api-key=' + channel.apiKey
+        // console.log(avilCheckUrl)
+        // console.log("https://live.jioreel.com/view/" + channelId + '?api-key=' + channel.apiKey)
+        // const avilCheckResponse = await fetch(avilCheckUrl, {
+        //   method: 'POST',
+        //   headers,
+        //   body: JSON.stringify({"state": "enabled"}),
+        // })
+        // const avilCheckData = await avilCheckResponse.text()
+        // if(![200,401].includes(avilCheckData.status)) throw new Error('Channel not available: ' + avilCheckData.status + ' - ' + avilCheckData)
+        // console.log('CHANNEL IS AVAILABLE!')
+      }
+  
+      console.log('FINISHED UPDATING ALL CLUSTERS')
+    } catch (e) {
+      console.error(e)
     }
-
-    console.log('FINISHED UPDATING ALL CLUSTERS')
-  } catch (e) {
-    console.error(e)
   }
 }
 
